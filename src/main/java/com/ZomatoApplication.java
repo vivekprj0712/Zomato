75c9042e23973537f0b58b34b7f572fd36f3b250
vivekprj0712 <prajapativivek949@gmail.com>
2024-09-24 18:38:06 +0530

    config.

---
+++ src/main/java/com/ZomatoApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoApplication.class, args);
+	}
+
+}
