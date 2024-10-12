f7eae4cd6b53762ed4e0e94716519440b5871e64
vivekprj0712 <prajapativivek949@gmail.com>
2024-10-12 15:14:39 +0530

    MenuEntity.

--- src/main/java/com/Entity/MenuEntity.java
+++ src/main/java/com/Entity/MenuEntity.java
@@ -1,11 +1,14 @@
 package com.Entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Data;
@@ -27,5 +30,8 @@ public class MenuEntity {
 	@ManyToOne
 	@JoinColumn(name = "restaurantId")
 	RestaurantEntity restaurant;
+	
+	@OneToMany(mappedBy = "menu")
+	List<ItemEntity> itemEntity;
 
 }
