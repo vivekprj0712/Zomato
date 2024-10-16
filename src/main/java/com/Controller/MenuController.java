035f0c60cca4c01f8afbd3e608ba36216fee6287
vivekprj0712 <prajapativivek949@gmail.com>
2024-10-16 15:53:24 +0530

    add ResponseEntity & RequestMapping.

--- src/main/java/com/Controller/MenuController.java
+++ src/main/java/com/Controller/MenuController.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Entity.MenuEntity;
 import com.Repository.MenuRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
+@RequestMapping("/api/private/menu")
 public class MenuController {
 
 	@Autowired
@@ -24,38 +28,42 @@ public class MenuController {
 	
 	// add menu
 	@PostMapping("menus")
-	public MenuEntity addMenu(@RequestBody MenuEntity menuEntity) {
+	public ResponseEntity<String> addMenu(@RequestBody MenuEntity menuEntity) {
 		menuRepository.save(menuEntity);
-		return menuEntity;
+		return new ResponseEntity<>("Record Added Successfully",HttpStatus.CREATED);
 	}
 	
 	//red All menus
 	@GetMapping("menus")
-	public List<MenuEntity> getAllMenus() {
+	public ResponseEntity<?> getAllMenus() {
 		List<MenuEntity> menus = menuRepository.findAll();
-		return menus;
+		if(menus.isEmpty()) {
+			return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
+		}else {
+			return new ResponseEntity<>(menus,HttpStatus.OK);
+		}
 	}
 	
 	//read Menu By Id
 	@GetMapping("menus/{menuId}")
-	public MenuEntity getMenuById(@PathVariable("menuId") Integer menuId) {
+	public ResponseEntity<?> getMenuById(@PathVariable("menuId") Integer menuId) {
 		Optional<MenuEntity> menu = menuRepository.findById(menuId);
 		if (menu.isEmpty()) {
-			return null;
+			return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
 		}else {
 			MenuEntity menuEntity = menu.get();
-			return menuEntity;
+			return new ResponseEntity<>(menuEntity,HttpStatus.OK);
 		}
 	}
 	
 	//read menu By RestaurantId
 	@GetMapping("menubyrestaurantid/{restaurantId}")
-	public Optional<MenuEntity> getMenuByRestaurantId(@PathVariable("restaurantId") Integer restaurantId) {
+	public ResponseEntity<?> getMenuByRestaurantId(@PathVariable("restaurantId") Integer restaurantId) {
 		Optional<MenuEntity> menus = menuRepository.findById(restaurantId);
 		if (menus.isEmpty()) {
-			return null;
+			return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
 		}else {
-			return menus;
+			return new ResponseEntity<>(menus,HttpStatus.OK);
 		}
 	}
 	
@@ -71,36 +79,33 @@ public class MenuController {
 
 	//read menu by Active
 	@GetMapping("menubyactive/{active}")
-	public List<MenuEntity> getMenuByActive(@PathVariable("active") Integer active) {
+	public ResponseEntity<?> getMenuByActive(@PathVariable("active") Integer active) {
 		List<MenuEntity> menus = menuRepository.findByActive(active);
 		if (menus.isEmpty()) {
-			return null;
+			return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
 		}else {
-			return menus;
+			return new ResponseEntity<>(menus,HttpStatus.OK);
+
 		}
 	}
 	
 	//delete Menu by Id
 	@DeleteMapping("menus/{menuId}")
-	public MenuEntity deleteMenu(@PathVariable("menuId") Integer menuId , MenuEntity menuEntity) {
+	public ResponseEntity<String> deleteMenu(@PathVariable("menuId") Integer menuId , MenuEntity menuEntity) {
 		Optional<MenuEntity> menu = menuRepository.findById(menuId);
 		if(menu.isEmpty()) {
-			return null;
+			return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
 		}else {
 			menuRepository.deleteById(menuId);
-			return menuEntity;
+			return new ResponseEntity<>("Record Deleted Successfully",HttpStatus.OK);
 		}
 	}
 	
 	//update menu by Id
 	@PutMapping("menus/{menuId}")
-	public MenuEntity updateMenu(@PathVariable("menuId") Integer menuId , @RequestBody MenuEntity menuEntity) {
-		Optional<MenuEntity> menu = menuRepository.findById(menuId);
-		if(menu.isEmpty()) {
-			return null;
-		}else {
+	public ResponseEntity<String> updateMenu(@PathVariable("menuId") Integer menuId , @RequestBody MenuEntity menuEntity) {
+		
 			menuRepository.save(menuEntity);
-			return menuEntity;
-		}
+			return new ResponseEntity<>("Record Updated Successfully",HttpStatus.OK);
 	}
 }
