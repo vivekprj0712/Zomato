3f00d634283672fe4479b8e51a1e104489ba2c26
vivekprj0712 <prajapativivek949@gmail.com>
2024-10-16 15:53:56 +0530

    add ResponseEntity & RequestMapping.

--- src/main/java/com/Controller/RestaurantController.java
+++ src/main/java/com/Controller/RestaurantController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Entity.RestaurantEntity;
@@ -13,6 +15,7 @@ import com.Repository.RoleRepository;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +25,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
+@RequestMapping("/api/private/restaurant")
 public class RestaurantController {
 
 	@Autowired
@@ -30,77 +34,68 @@ public class RestaurantController {
 	@Autowired
 	RoleRepository roleRepository;
 	
-	//add Restaurant
-	@PostMapping("restaurants")
-	public RestaurantEntity addRestaurant(@RequestBody RestaurantEntity restaurantEntity) {
-		Optional<RoleEntity> role = roleRepository.findById(3);
-		restaurantEntity.setRole(role.get());
-		restaurantRepository.save(restaurantEntity);
-		return restaurantEntity;
-	}
 	
 	// Read All Restaurant
 	@GetMapping("restaurants")
-	public List<RestaurantEntity> getAllRestaurants() {
+	public ResponseEntity<?> getAllRestaurants() {
 		List<RestaurantEntity> restaurants = restaurantRepository.findAll();
 		if(restaurants.isEmpty()) {
-			return null;
+			return new ResponseEntity<>("Record not found",HttpStatus.BAD_REQUEST);
 		}else {
-			return restaurants;
+			return new ResponseEntity<>(restaurants,HttpStatus.OK);
 		}
 	}
 	
 	// Read Restaurant by Id
 		@GetMapping("restaurants/{restaurantId}")
-		public RestaurantEntity getRestaurantsById(@PathVariable("restaurantId") Integer restaurantId) {
+		public ResponseEntity<?> getRestaurantsById(@PathVariable("restaurantId") Integer restaurantId) {
 			Optional<RestaurantEntity> restaurants = restaurantRepository.findById(restaurantId);
 			if(restaurants.isEmpty()) {
-				return null;
+				return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
 			}else {
 				RestaurantEntity	restaurantEntity = restaurants.get();
-				return restaurantEntity;
+				return new ResponseEntity<>(restaurantEntity,HttpStatus.OK);
 			}
 		}
 	
 	// Read All Restaurant By Active
 	@GetMapping("restaurantsbyactive/{active}")
-	public List<RestaurantEntity> getAllRestaurantsByActive(@PathVariable("active") Integer active ) {
+	public ResponseEntity<?> getAllRestaurantsByActive(@PathVariable("active") Integer active ) {
 		List<RestaurantEntity> restaurants = restaurantRepository.findByActive(active);
 		if(restaurants.isEmpty()) {
-			return null;
+			return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
 		}else {
-			
-			return restaurants;
+			return new ResponseEntity<>(restaurants,HttpStatus.OK);
 		}
 	}
 
-	// Read All Restaurant By Active and Pincode
+	// Read All Restaurant By Active and PinCode
 	@GetMapping("restaurants/{active}/{pincode}")
-	public List<RestaurantEntity> getAllRestaurantsByActive(@PathVariable("active") Integer active , @PathVariable("pincode") Integer pincode) {
+	public ResponseEntity<?> getAllRestaurantsByActive(@PathVariable("active") Integer active , @PathVariable("pincode") Integer pincode) {
 		List<RestaurantEntity> restaurants = restaurantRepository.findByActiveAndPincode(active,pincode);
 		if(restaurants.isEmpty()) {
-			return null;
+			return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
 		}else {
-			return restaurants;
+			return new ResponseEntity<>(restaurants,HttpStatus.OK);
 		}
 	}
 	
 	// Delete Restaurant By Id
 	@DeleteMapping("restaurants/{restaurantId}")
-	public RestaurantEntity deleteRestaurantById(@PathVariable("restaurantId") Integer restaurantId , RestaurantEntity restaurantEntity) {
+	public ResponseEntity<?> deleteRestaurantById(@PathVariable("restaurantId") Integer restaurantId) {
 		Optional<RestaurantEntity> restaurant = restaurantRepository.findById(restaurantId);
 		if(restaurant.isEmpty()) {
-			return null;
+			return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
 		}else {
 			restaurantRepository.deleteById(restaurantId);
-			return restaurantEntity;
+			return new ResponseEntity<>("Record Deleted Successfully",HttpStatus.OK);
 		}
 	}
 	
 	// Update Restaurant by Id
 	@PutMapping("restaurants/{restaurantId}")
-	public RestaurantEntity updateRestaurant(@PathVariable("restaurantId") Integer restaurantId, @RequestBody RestaurantEntity restaurantEntity) {
+	public ResponseEntity<String> updateRestaurant(@PathVariable("restaurantId") Integer restaurantId, @RequestBody RestaurantEntity restaurantEntity) {
 			restaurantRepository.save(restaurantEntity);
-			return restaurantEntity;
+			return new ResponseEntity<>("Record Updated Successfully",HttpStatus.OK);
 		}
 }
