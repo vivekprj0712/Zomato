08f8d84a13afefed44191137803ca736c9799689
vivekprj0712 <prajapativivek949@gmail.com>
2024-10-16 15:53:15 +0530

    add ResponseEntity & RequestMapping.

--- src/main/java/com/Controller/ItemController.java
+++ src/main/java/com/Controller/ItemController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Entity.ItemEntity;
@@ -11,6 +13,7 @@ import com.Repository.ItemRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
+@RequestMapping("/api/private/item")
 public class ItemController {
 
 	@Autowired
@@ -26,61 +30,61 @@ public class ItemController {
 	
 	//add item
 	@PostMapping("items")
-	public ItemEntity addItem(@RequestBody ItemEntity itemEntity) {	
+	public ResponseEntity<String> addItem(@RequestBody ItemEntity itemEntity) {	
 		itemRepository.save(itemEntity);
-		return itemEntity;
+		return new ResponseEntity<>("Record Added Successfully",HttpStatus.CREATED);
 	}
 	
 	//get All Items
 	@GetMapping("items")
-	public List<ItemEntity> getAllItems() {
+	public ResponseEntity<?> getAllItems() {
 		List<ItemEntity> items = itemRepository.findAll();
 		if(items.isEmpty()) {
-			return null;
+			return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
 		}else {
-			return items;
+			return new ResponseEntity<>(items,HttpStatus.OK);
 		}
 	}
 	
 	//get Items by Id
 		@GetMapping("items/{itemId}")
-		public ItemEntity getItemById(@PathVariable("itemId") Integer itemId) {
+		public ResponseEntity<?> getItemById(@PathVariable("itemId") Integer itemId) {
 			Optional<ItemEntity> items = itemRepository.findById(itemId);
 			if(items.isEmpty()) {
-				return null;
+				return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
 			}else {
 				ItemEntity item = items.get();
-				return item;
+				return new ResponseEntity<>(item,HttpStatus.OK);
 			}
 		}
 	
 		//get Items by Menu Id
 		@GetMapping("itembymenuid/{menuId}")
-		public ItemEntity getItemByMenuId(@PathVariable("menuId") Integer menuId) {
+		public ResponseEntity<?> getItemByMenuId(@PathVariable("menuId") Integer menuId) {
 			Optional<ItemEntity> items = itemRepository.findById(menuId);
 			if(items.isEmpty()) {
-				return null;
+				return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
 			}else {
 				ItemEntity item = items.get();
-				return item;
+				return new ResponseEntity<>(item,HttpStatus.OK);
 			}
 		}
 		//delete Items by Id
 		@DeleteMapping("items/{itemId}")
-		public ItemEntity deleteItemById(@PathVariable("itemId") Integer itemId , ItemEntity itemEntity) {
+		public ResponseEntity<String> deleteItemById(@PathVariable("itemId") Integer itemId , ItemEntity itemEntity) {
 			Optional<ItemEntity> item = itemRepository.findById(itemId);
 			if(item.isEmpty()) {
-				return null;
+				return new ResponseEntity<>("Record not found",HttpStatus.NOT_FOUND);
 			}else {
 				itemRepository.deleteById(itemId);
-				return itemEntity;
+				return new ResponseEntity<>("Record Deleted Successfully",HttpStatus.OK);
 			}
 		}
 	
 		//update Items by Id
 		@PutMapping("items/{itemId}")
-		public ItemEntity updateItemById(@PathVariable("itemId") Integer itemId ,@RequestBody ItemEntity itemEntity) {
+		public ResponseEntity<String> updateItemById(@PathVariable("itemId") Integer itemId ,@RequestBody ItemEntity itemEntity) {
 			itemRepository.save(itemEntity);
-			return itemEntity;
+			return new ResponseEntity<>("Record Updated Successfully",HttpStatus.OK);
 		}
 }
