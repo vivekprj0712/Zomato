8e1fec517a0208d06b83ac6368255d6bd240fff2
vivekprj0712 <prajapativivek949@gmail.com>
2024-10-18 18:10:20 +0530

    create PasswordEncoder function.

--- src/main/java/com/ZomatoApplication.java
+++ src/main/java/com/ZomatoApplication.java
@@ -2,12 +2,21 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ZomatoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZomatoApplication.class, args);
+	}
+	
+	@Bean
+	public PasswordEncoder createPasswordEncoder() {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		return passwordEncoder;
 	}
 
 }
