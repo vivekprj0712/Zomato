45136c0121462dbcee79ca12a3a68f767c7fe555
vivekprj0712 <prajapativivek949@gmail.com>
2024-10-18 18:09:31 +0530

    security password.

--- src/main/java/com/Controller/SessionController.java
+++ src/main/java/com/Controller/SessionController.java
@@ -1,10 +1,13 @@
 package com.Controller;
 
+import java.util.HashMap;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Dto.LoginDto;
 import com.Entity.CustomerEntity;
 import com.Entity.RestaurantEntity;
 import com.Entity.RoleEntity;
@@ -40,71 +44,97 @@ public class SessionController {
 	RestaurantRepository restaurantRepository;
 	
 	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Autowired
 	MailerService mailerService;
 	
-	// Signup & add customer
+	// Sign up & add customer
 	@PostMapping("customersignup")
 	public ResponseEntity<String>  customerSignup(@RequestBody CustomerEntity customerEntity) {
 
 		Optional<RoleEntity> role = roleRepository.findById(2);
 		CustomerEntity customerEmail = customerRepository.findByEmail(customerEntity.getEmail());
 		//Email Present?
-		if(customerEmail == null) {
+		if(customerEmail != null) {
 			return new ResponseEntity<>("Email already exists", HttpStatus.CONFLICT);
 		}if(role.isEmpty()) {
 			return new ResponseEntity<>("Customer has no roles assigned", HttpStatus.FORBIDDEN);
 		}
 		else {
-			customerEntity.setRole(role.get());
-			customerRepository.save(customerEntity);
-			return new ResponseEntity<>("Customer registered Successfully", HttpStatus.CREATED);
+			
+			// read plain text password
+				String plainPassword = customerEntity.getPassword();
+						
+			// encrypt plain text password
+				String encPassword = passwordEncoder.encode(plainPassword);
+				customerEntity.setPassword(encPassword);
+				
+				customerEntity.setRole(role.get());
+				customerRepository.save(customerEntity);
+				return new ResponseEntity<>("Customer registered Successfully", HttpStatus.CREATED);
 		
 		}
 	}
 	
-	//Signup & add Restaurant
+	//Sign up & add Restaurant
 		@PostMapping("restaurants")
 		public ResponseEntity<String> addRestaurant(@RequestBody RestaurantEntity restaurantEntity) {
 			Optional<RoleEntity> role = roleRepository.findById(3);
 			if(role.isEmpty()) {
 				return new ResponseEntity<>("Restaurant has no roles assigned", HttpStatus.FORBIDDEN);
 			}else {
+				
+				// read plain text password
+				String plainPassword = restaurantEntity.getPassword();
+						
+				// encrypt plain text password
+				String encPassword = passwordEncoder.encode(plainPassword);
+				
+				restaurantEntity.setPassword(encPassword);
 				restaurantEntity.setRole(role.get());
 				restaurantRepository.save(restaurantEntity);
 				return new ResponseEntity<>("Restaurant registered Successfully", HttpStatus.CREATED);
 			}
 		}
 	
-	@GetMapping("authentication/{email}/{password}")   // Pending...
-	public ResponseEntity<String> authentication(@PathVariable("email") String email , @PathVariable("password") String password) {
+	@PostMapping("authentication/{email}/{password}")
+	public ResponseEntity<?> authentication(@RequestBody LoginDto loginDto) {
 		
-		   CustomerEntity loggedInCustomer = customerRepository.findByEmail(email);
-		   RestaurantEntity loggedInRestaurant = restaurantRepository.findByEmail(email);
+		   CustomerEntity loggedInCustomer = customerRepository.findByEmail(loginDto.getEmail());
+		   RestaurantEntity loggedInRestaurant = restaurantRepository.findByEmail(loginDto.getEmail());
 
+		   String password = loginDto.getPassword();
 		if(loggedInCustomer == null && loggedInRestaurant == null) {
 			return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
 		}else {
 			
 			if(loggedInCustomer != null) {
 				
-				if(!loggedInCustomer.getPassword().matches(password)) {
+				if(passwordEncoder.matches(password, loggedInCustomer.getPassword())) {
 					return new ResponseEntity<>("Password not matches", HttpStatus.UNAUTHORIZED);
 				}else if(loggedInCustomer.getRole() == null) {
 					return new ResponseEntity<>("Customer has no roles assigned", HttpStatus.FORBIDDEN);
 				}else if(loggedInCustomer.getRole().getRoleId() == 2) {
 					System.out.println(loggedInCustomer.getRole());
 					System.out.println();
-					return new ResponseEntity<>("Login SuccessFul", HttpStatus.OK);
+					HashMap<String, Object> map = new HashMap<>();
+					map.put("restaurants", restaurantRepository.findAll());
+					map.put("customer", loggedInCustomer);
+					map.put("authToken", "customer");
+					return new ResponseEntity<>(map, HttpStatus.OK);
 				}
 			}else if(loggedInRestaurant != null) {
 					
-					if(!loggedInRestaurant.getPassword().matches(password)) {
+					if(passwordEncoder.matches(password, loggedInRestaurant.getPassword())) {
 						return new ResponseEntity<>("Password not matches", HttpStatus.UNAUTHORIZED);
 					}else if(loggedInRestaurant.getRole() == null) {
 						return new ResponseEntity<>("Restaurant has no roles assigned", HttpStatus.FORBIDDEN);
 					}else if(loggedInRestaurant.getRole().getRoleId() == 3) {
 						System.out.println(loggedInRestaurant.getRole());
-						return new ResponseEntity<>("Login Is SuccessFul", HttpStatus.OK);
+						HashMap<String, Object> map = new HashMap<>();
+						map.put("authToken", "restaurant");
+						return new ResponseEntity<>(map, HttpStatus.OK);
 					}
 				}
 			}
@@ -142,8 +172,7 @@ public class SessionController {
 			}else if(dbCustomer.getOtp().intValue() != otp || dbCustomer.getOtp() == null)
 				{
 				return new ResponseEntity<>("OTP does not match", HttpStatus.UNAUTHORIZED);
-				}
-			else {
+			}else {
 				dbCustomer.setPassword(password);
 				dbCustomer.setOtp(-1);
 				customerRepository.save(dbCustomer);
