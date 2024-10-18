c1fa958d379b7d5881082f7abe931fdc91adb3a1
vivekprj0712 <prajapativivek949@gmail.com>
2024-10-18 18:18:47 +0530

    ZomatoApplicationTests.

--- src/test/java/com/ZomatoApplicationTests.java
+++ src/test/java/com/ZomatoApplicationTests.java
@@ -1,7 +1,12 @@
 package com;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootTest
 class ZomatoApplicationTests {
@@ -10,4 +15,10 @@ class ZomatoApplicationTests {
 	void contextLoads() {
 	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoApplication.class, args);
+	}
+	
+
+		
 }
